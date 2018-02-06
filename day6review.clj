26eb08ab39a4f1524c6e79c6478fe4268b99839f
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2018-02-05 22:46:21 -0500

    Update day6review.clj

--- day6review.clj
+++ day6review.clj
@@ -1,16 +1,26 @@
 (use '[clojure.string :only (split triml)])
-;incomplete
-(
-        let [
-          n_t (read-line) 
-          n (Integer/parseInt n_t)
-        ]
-        (defn printpart [acc] 
-        (if (< acc n)
-            (let [ a  (map-indexed vector (read-line))]
-            (println a )
-            (recur (+ 1 acc))
-        )))
-        (printpart 0)
 
+(
+    let [
+      n_t (read-line) 
+      n (Integer/parseInt n_t)
+    ]
+    (defn oddfirst? [x]
+        (odd? (first x)))
+    (defn evenfirst? [x]
+        (even? (first x)))
+    (defn printpart [acc] 
+        (if (< acc n)
+            (let [
+                  line  (map-indexed vector (read-line))
+                  odds (filter oddfirst? line)
+                  evens (filter evenfirst? line)
+                  oddletters (map peek odds)
+                  evenletters (map peek evens)
+                  ]
+            (println 
+                (str (clojure.string/join evenletters)
+                    " " (clojure.string/join oddletters)))
+            (recur (+ 1 acc)))))
+    (printpart 0)
 )
